04b1b9ca8af1013b93c3ef937e7501a0271b9d5d
ArseniyDev <jkl@usa.com>
2025-05-13 14:48:45 +0300

    test

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250513_114829 from './20250513_114829';
+
+export const migrations = [
+  {
+    up: migration_20250513_114829.up,
+    down: migration_20250513_114829.down,
+    name: '20250513_114829'
+  },
+];
